086ff9f304a5d788acacb64ad8a25caebae951ad
everysick <s.wakeup31@gmail.com>
2015-03-21 02:59:29 +0900

    create rule of Finite Automaton

---
+++ regex_expression/fa_rule.rb
@@ -0,0 +1,9 @@
+class FaRule < Struct.new(:state, :character, :next_state)
+  def applies_to?(state, character)
+    self.state == state && self.character == character
+  end
+
+  def follow
+    next_state
+  end
+end
