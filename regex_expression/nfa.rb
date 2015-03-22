a43552708b0c659f8bf31111e98882f0b5542413
everysick <s.wakeup31@gmail.com>
2015-03-22 17:38:23 +0900

    Support to read characters

--- regex_expression/nfa.rb
+++ regex_expression/nfa.rb
@@ -2,4 +2,14 @@ class Nfa < Struct.new(:current_states, :accept_states, :rulebook)
   def accepting?
     (current_states & accept_states).any?
   end
+
+  def read_character(character)
+    self.current_states = rulebook.next_states(current_states, character)
+  end
+
+  def read_string(string)
+    string.chars.each do |character|
+      read_character(character)
+    end
+  end
 end
