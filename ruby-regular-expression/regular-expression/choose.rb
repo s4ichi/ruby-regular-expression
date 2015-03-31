5bb33434b84ae180b22f8902d7360410ad890d9a
everysick <s.wakeup31@gmail.com>
2015-03-31 12:25:36 +0900

    Add to_nfa_design method in Choose class

--- ruby-regular-expression/regular-expression/choose.rb
+++ ruby-regular-expression/regular-expression/choose.rb
@@ -10,5 +10,20 @@ class Choose < Struct.new(:first, :second)
   def precedence
     0
   end
-end
 
+  def to_nfa_design
+    first_nfa_design = first.to_nfa_design
+    second_nfa_design = second.to_nfa_design
+
+    start_state = Object.new
+    accept_states = first_nfa_design.accept_states + second_nfa_design.accept_states
+
+    rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+    extra_rules = [first_nfa_design, second_nfa_design].map do |nfa_design|
+      FaRule.new(start_state, nil, nfa_design.start_state)
+    end
+    rulebook = NfaRulebook.new(rules + extra_rules)
+
+    NfaDesign.new(start_state, accept_states, rulebook)
+  end
+end
