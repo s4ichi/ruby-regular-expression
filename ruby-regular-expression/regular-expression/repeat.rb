2a9faa782e552726784807f66ed72b92847eff95
everysick <s.wakeup31@gmail.com>
2015-03-31 12:34:30 +0900

    Add to_nfa_design method in Repeat class

--- ruby-regular-expression/regular-expression/repeat.rb
+++ ruby-regular-expression/regular-expression/repeat.rb
@@ -8,5 +8,20 @@ class Repeat < Struct.new(:pattern)
   def precedence
     2
   end
+
+  def to_nfa_design
+    pattern_nfa_design = pattern.to_nfa_design
+
+    start_state = Object.new
+    accept_states = pattern_nfa_design.accept_states + [start_state]
+
+    rules = pattern_nfa_design.rulebook.rules
+    extra_rules = pattern_nfa_design.accept_states.map do |accept_state|
+      FaRule.new(accept_state, nil, pattern_nfa_design.start_state)
+    end + [FaRule.new(start_state, nil, pattern_nfa_design.start_state)]
+    rulebook = NfaRulebook.new(rules + extra_rules)
+
+    NfaDesign.new(start_state, accept_states, rulebook)
+  end
 end
 
