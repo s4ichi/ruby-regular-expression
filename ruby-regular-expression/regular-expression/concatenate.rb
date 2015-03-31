b43cbf8b67dad9a94f9e99db48e01331e132a392
everysick <s.wakeup31@gmail.com>
2015-03-31 12:18:55 +0900

    Add to_nfa_design mthod in Concatenate class

--- ruby-regular-expression/regular-expression/concatenate.rb
+++ ruby-regular-expression/regular-expression/concatenate.rb
@@ -10,5 +10,21 @@ class Concatenate < Struct.new(:first, :second)
   def precedence
     1
   end
+
+  def to_nfa_design
+    first_nfa_design = first.to_nfa_design
+    second_nfa_design = second.to_nfa_design
+
+    start_state = first_nfa_design.start_state
+    accept_states = second_nfa_design.accept_states
+
+    rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+    extra_rules = first_nfa_design.accept_states.map do |state|
+      FaRule.new(state, nil ,second_nfa_design.start_state)
+    end
+    rulebook = NfaRulebook.new(rules + extra_rules)
+
+    NfaDesign.new(start_state, accept_states, rulebook)
+  end
 end
 
