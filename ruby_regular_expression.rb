ae34ac58badc4f51da0fc47d8de67f253c894454
everysick <s.wakeup31@gmail.com>
2015-03-31 11:29:34 +0900

    fix bug

--- ruby_regular_expression.rb
+++ ruby_regular_expression.rb
@@ -2,7 +2,7 @@ require 'set'
 require 'treetop'
 
 # non-deterministic finite automaton
-require './ruby-regular_expression/nfa/nfa_rulebook'
-require './ruby-regular_expression/nfa/fa_rule'
-require './ruby-regular_expression/nfa/nfa'
-require './ruby-regular_expression/nfa/nfa_design'
+require './ruby-regular-expression/nfa/nfa_rulebook'
+require './ruby-regular-expression/nfa/fa_rule'
+require './ruby-regular-expression/nfa/nfa'
+require './ruby-regular-expression/nfa/nfa_design'
