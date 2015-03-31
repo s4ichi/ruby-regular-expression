773f69b2630a0d3dd79c86444e88867ba5b545ef
everysick <s.wakeup31@gmail.com>
2015-03-31 11:27:06 +0900

    Rename Directories and files

--- regex_expression.rb
+++
@@ -1,8 +0,0 @@
-require 'set'
-require 'treetop'
-
-# non-deterministic finite automaton
-require './regex_expression/nfa_rulebook'
-require './regex_expression/fa_rule'
-require './regex_expression/nfa'
-require './regex_expression/nfa_design'
---
+++ ruby_regular_expression.rb
@@ -0,0 +1,8 @@
+require 'set'
+require 'treetop'
+
+# non-deterministic finite automaton
+require './ruby-regular_expression/nfa/nfa_rulebook'
+require './ruby-regular_expression/nfa/fa_rule'
+require './ruby-regular_expression/nfa/nfa'
+require './ruby-regular_expression/nfa/nfa_design'
